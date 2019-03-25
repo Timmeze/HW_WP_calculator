5a0cc1c652e2822da4e74e71651c9344a50e375d
MezentsevTA <timur-mezentsev@yandex.ru>
2019-03-25 11:51:58 +0300

    result's output bug fixed

--- app/lib.py
+++ app/lib.py
@@ -1,4 +1,4 @@
-def wallpaper_calc(lengh, widgh, heigh, wall_paper):
+def wallpaper_calc(lengh, heigh, widgh, wallpaper_width):
     """
     >>> wallpaper_calc(5, 6, 2.5, 1.04)
     9
@@ -6,5 +6,5 @@ def wallpaper_calc(lengh, widgh, heigh, wall_paper):
     """
     perimetr_round = 1 # округление значение кол-во полотнищ после деления перимера на ширину полотнища
     reserve_rate = 2 # значение общего запаса обоев
-    result = round((((lengh + widgh) * 2 // wall_paper + perimetr_round) / (10 // (heigh + 0.1))) + reserve_rate)
+    result = round((((lengh + widgh) * 2 // wallpaper_width + perimetr_round) / (10 // (heigh + 0.1))) + reserve_rate)
     return result
--- app/main.py
+++ app/main.py
@@ -13,10 +13,9 @@ def main():
         height = request.args.get('height')
         width = request.args.get('width')
         wallpaper_width = request.args.get('wallpaper_width')
-        if lenght and height:
-            if width and wallpaper_width:
-                result = wallpaper_calc(float(lenght), float(height), float(width), float(wallpaper_width))
-                return render_template('calc.html', title="Wallpaper calculator", result=result)
+        if lenght and height and width and wallpaper_width:
+            res = wallpaper_calc(float(lenght), float(height), float(width), float(wallpaper_width))
+            return render_template('calc.html', title="Wallpaper calculator", res=res, lenght=lenght, height=height, width=width, wallpaper_width=wallpaper_width)
         return render_template('calc.html', title="Wallpaper calculator",)
 
     app.run(port=9875, debug=True)
